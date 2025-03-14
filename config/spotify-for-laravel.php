f7fd457b6a28ed32870ec292878bac21ed474e9d
KieranLProctor <kieran.lee.proctor@gmail.com>
2025-03-14 23:00:48 +1100

    feat: functional classes complete

--- config/spotify-for-laravel.php
+++ config/spotify-for-laravel.php
@@ -1,7 +1,27 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | API Base URL
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define the base URL of the Spotify API.
+    |
+    */
+
     'api_url' => 'https://api.spotify.com/v1',
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define the required settings depending on which auth flow
+    | you are using.
+    |
+    */
 
     'auth' => [
         'client_id' => env('SPOTIFY_CLIENT_ID'),
@@ -10,6 +30,17 @@ return [
         'scope' => [],
         'show_dialog' => false,
     ],
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Config
+    |--------------------------------------------------------------------------
+    |
+    | You may define a default country, locale and market that will be used
+    | for your Spotify API requests.
+    |
+    */
 
     'default_config' => [
         'country' => null,
