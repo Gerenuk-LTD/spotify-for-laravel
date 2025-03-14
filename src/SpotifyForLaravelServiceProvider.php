6467e33b107fdc84ce3f052d15e4030e75a8ebc2
KieranLProctor <kieran.lee.proctor@gmail.com>
2025-03-15 00:25:58 +1100

    feat: add get requests

--- src/SpotifyForLaravelServiceProvider.php
+++ src/SpotifyForLaravelServiceProvider.php
@@ -13,4 +13,28 @@ class SpotifyForLaravelServiceProvider extends PackageServiceProvider
             ->name('spotify-for-laravel')
             ->hasConfigFile();
     }
+
+    public function packageRegistered(): void
+    {
+        $this->app->singleton(Spotify::class, function () {
+            $defaultConfig = [
+                'country' => config('spotify-for-laravel.default_config.country'),
+                'locale' => config('spotify-for-laravel.default_config.locale'),
+                'market' => config('spotify-for-laravel.default_config.market'),
+            ];
+
+            return new Spotify($defaultConfig);
+        });
+
+        $this->app->singleton(SpotifyAuth::class, function () {
+            $clientId = config('spotify-for-laravel.auth.client_id');
+            $clientSecret = config('spotify-for-laravel.auth.client_secret');
+
+            return new SpotifyAuth($clientId, $clientSecret);
+        });
+
+        $this->app->bind(SpotifyClient::class, function () {
+            return new SpotifyClient;
+        });
+    }
 }
