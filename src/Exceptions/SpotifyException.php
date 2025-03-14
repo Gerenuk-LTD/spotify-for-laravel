cce0851bc074acf28432f774fb298b6807a145c8
KieranLProctor <25559341+KieranLProctor@users.noreply.github.com>
2025-03-14 10:46:00 +0000

    Fix styling

--- src/Exceptions/SpotifyException.php
+++ src/Exceptions/SpotifyException.php
@@ -8,7 +8,7 @@ class SpotifyException extends Exception
 {
     protected mixed $apiResponse;
 
-    public function __construct(string $message = null, $code = 0, $apiResponse = null, ?Exception $previous = null)
+    public function __construct(?string $message = null, $code = 0, $apiResponse = null, ?Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
 
